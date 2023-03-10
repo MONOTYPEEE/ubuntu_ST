4a9ef97dd6cbb1b05555701ef5ca0c4179af0aaf
monotypeee <spair0039@gmail.com>
2023-03-10 12:21:52 +0900

    another file access

---
+++ fileAcc_sysCall.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main(){
+	int fd;
+	char pathname[] = "temp.txt";
+	
+	if((fd=open(pathname, O_RDWR, 0644)) == -1){
+		printf("nah err\n");
+		exit(1);
+	}
+	
+	close(fd);
+}
